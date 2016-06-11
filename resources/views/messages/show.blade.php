04591fe61dbde41496767da8b5f29bcfee0250f1
Xieyijian <xieyijian1997@qq.com>
2016-06-11 16:14:40 +0800

    Notification display improvement.

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -5,25 +5,27 @@
 @section('content')
 	<a href="/message">&lt;&lt;back</a>
 	<form class="form-horizontal">
-		<div class="form-group">
-			<label for="sender" class="col-md-2 control-label">From:</label>
-			<div class="col-md-10">
-				<p class="form-control-static">
-					@if ($message->sender_id != 0)
-						<a href="/user/{{ $message->sender_id }}">{{ $user::find($message->sender_id)->name }}</a>&lt;{{ $user::find($message->sender_id)->email }}&gt;
-					@else
-						System Notification
-					@endif
-				</p>
+		@if ($message->sender_id != 0)
+			<div class="form-group">
+				<label for="sender" class="col-md-2 control-label">From:</label>
+				<div class="col-md-10">
+					<p class="form-control-static">
+						@if ($message->sender_id != 0)
+							<a href="/user/{{ $message->sender_id }}">{{ $user::find($message->sender_id)->name }}</a>&lt;{{ $user::find($message->sender_id)->email }}&gt;
+						@else
+							System Notification
+						@endif
+					</p>
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<label for="receiver" class="col-md-2 control-label">To:</label>
-			<div class="col-md-10">
-				<p class="form-control-static"><a href="/user/{{ $message->receiver_id }}">{{ $user::find($message->receiver_id)->name }}</a>&lt;{{ $user::find($message->receiver_id)->email }}&gt;</p>
+			<div class="form-group">
+				<label for="receiver" class="col-md-2 control-label">To:</label>
+				<div class="col-md-10">
+					<p class="form-control-static"><a href="/user/{{ $message->receiver_id }}">{{ $user::find($message->receiver_id)->name }}</a>&lt;{{ $user::find($message->receiver_id)->email }}&gt;</p>
+				</div>
 			</div>
-		</div>
+		@endif
 
 		<div class="form-group">
 			<label for="subject" class="col-md-2 control-label">Subject:</label>
